4e743af7c470b0019adfd0756dbaa67421d6b30e
happy_panda <Jassinakhlaqi@hotmail.com>
2024-06-22 17:52:09 +0200

    feat(JAS-13): add exception handling

--- backend/src/main/java/com/company/project/entity/PlantDto.java
+++ backend/src/main/java/com/company/project/entity/PlantDto.java
@@ -1,18 +1,27 @@
 package com.company.project.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.UUID;
+import javax.validation.constraints.Size;
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlantDto {
 
-    private UUID id;
+    @Size(min=1, max = 255, message = "Name must be between 1 and 255 characters")
+    @Schema(description = "Name of the plant",
+            defaultValue = "No name available",
+            example = "Monstera")
     private String name;
+
+    @Size(max = 4096, message = "Description must be less than 4096 characters")
+    @Schema(description = "Description of the plant",
+            defaultValue = "No description available",
+            example = "This plant is a tropical plant that requires a lot of sunlight and water. It is a great plant for beginners.")
     private String description;
 }
