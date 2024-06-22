02542d8ac254c6f037d955595089f33847ba9930
happy_panda <Jassinakhlaqi@hotmail.com>
2024-06-22 14:13:44 +0200

    feat(JAS-12): initialize openApi to the project

--- backend/src/main/java/com/company/project/controllers/PlantController.java
+++ backend/src/main/java/com/company/project/controllers/PlantController.java
@@ -3,7 +3,18 @@ package com.company.project.controllers;
 import com.company.project.entity.PlantDto;
 import com.company.project.rest.RestApi;
 import com.company.project.services.PlantService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +22,32 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
+@Api(value = "Plant Management System")
 @RequiredArgsConstructor
 @RestController
 public class PlantController {
 
     private final PlantService plantService;
 
-
+    @ApiOperation(value = "View a list of available plants", response = List.class)
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(description = "View a list of available Plants", responses = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of plants"),
+            @ApiResponse(responseCode = "400", description = "Please provide a valid request body and check the request method", content = @Content(schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE)),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE)),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "The requested Plant could not be found", content = @Content(schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE)),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = Problem.class), mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE))}, extensions = {
+                @Extension(name = "awesomegardening", properties = {
+                        @ExtensionProperty(name = "resoource-id-type", value = "dynamic"),
+                        @ExtensionProperty(name = "resource-id", value = "plantsId"),
+                        @ExtensionProperty(name = "resource-id-description", value = "The unique identifier for a plant"),
+                        @ExtensionProperty(name = "resource-type", value = "plant"),
+                        @ExtensionProperty(name = "resource-type-description", value = "The type of object plant"),
+                        @ExtensionProperty(name = "action", value = "[\"project.settings.read\"]", parseValue = true)
+                })
+        }
+    )
     @GetMapping(RestApi.PLANTS)
     public ResponseEntity<List<PlantDto>> getPlants() {
         List<PlantDto> plantList = plantService.findAllPlants();
